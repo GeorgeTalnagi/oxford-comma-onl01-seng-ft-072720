847a6db90a2846a292efef3e2c46727d500c25cb
George Talnagi <gtalnagi@yahoo.com>
2020-07-30 18:33:27 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,8 +2,7 @@ def oxford_comma(array)
   if array.length > 1
     array.each do |word|
       if word == array[-1]
-          word.push do "and"
-          end
+          word "and"
       else
         word << ", "
       end
