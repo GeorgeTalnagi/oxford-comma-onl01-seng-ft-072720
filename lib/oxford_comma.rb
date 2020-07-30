247695260ea424958f16d85c324a96dc5fa5747a
George Talnagi <gtalnagi@yahoo.com>
2020-07-30 18:41:09 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  if array.length > 1
+    array.each do |word|
+      if word == array[-1]
+          word.prepend "and "
+      elsif array.length == 2
+          word << " "
+      else
+        word << ", "
+      end
+    end
+  end
+  array.join()
 end
